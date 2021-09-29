b3efc2c5b70d45e0b4701ad46e127df30b000e06
ellakrechmer <ekrechmer20@stuy.edu>
2021-09-28 22:50:42 -0400

    changed percentages to floats, multiplied by 10 so you can work with random integers

--- 06_py-csv/k06occupations.py
+++ 06_py-csv/k06occupations.py
@@ -1,11 +1,13 @@
 import csv
+from random import *;
 with open('occupations.csv', newline='') as csvfile:
     spamreader=csv.reader(csvfile, delimiter=',')
     data={}
-    counter=0;
     for row in spamreader:
         data[row[0]]=row[1]
     keyslist=list(data)
     data.pop(keyslist[0])
     data.pop(keyslist[len(keyslist)-1])
+    for job in data:
+        data[job]=float(data[job])*10
 print(data)
