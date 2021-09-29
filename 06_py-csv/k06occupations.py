1b541287f0f7fa06f506dfefe4dc46d55e232deb
ellakrechmer <ekrechmer20@stuy.edu>
2021-09-28 22:56:37 -0400

    completed and tested code

--- 06_py-csv/k06occupations.py
+++ 06_py-csv/k06occupations.py
@@ -1,13 +1,27 @@
 import csv
 from random import *;
 with open('occupations.csv', newline='') as csvfile:
+    #read in data
     spamreader=csv.reader(csvfile, delimiter=',')
+    #add to dictionary
     data={}
     for row in spamreader:
         data[row[0]]=row[1]
+    #remove first and last row
     keyslist=list(data)
     data.pop(keyslist[0])
     data.pop(keyslist[len(keyslist)-1])
+    #make the percentages floats, then multiply by 10 so they're easier to work with later
     for job in data:
         data[job]=float(data[job])*10
-print(data)
+
+    #randomly print out job based on percentages
+    n=randrange(1000)
+    start=0
+    end=0
+    for job in data:
+        #this sets the range we're looking at for how likely it is to happen
+        start=end
+        end+=data[job]
+        if (start<=n<end):
+            print(job)
