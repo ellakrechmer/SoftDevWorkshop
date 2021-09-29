12f9470a9fc4a5cffe9bb5bf651c47d73590127f
ellakrechmer <ekrechmer20@stuy.edu>
2021-09-28 22:40:24 -0400

    put all the data in a dictionary, not including the first and last rows

--- 06_py-csv/k06occupations.py
+++ 06_py-csv/k06occupations.py
@@ -1,5 +1,11 @@
 import csv
 with open('occupations.csv', newline='') as csvfile:
     spamreader=csv.reader(csvfile, delimiter=',')
+    data={}
+    counter=0;
     for row in spamreader:
-        print(row)
+        data[row[0]]=row[1]
+    keyslist=list(data)
+    data.pop(keyslist[0])
+    data.pop(keyslist[len(keyslist)-1])
+print(data)
