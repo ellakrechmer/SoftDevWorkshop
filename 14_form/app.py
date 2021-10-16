201c83505d1d6076ca1cbb31764080e7f3d55ed8
ellakrechmer <ekrechmer20@stuy.edu>
2021-10-16 16:38:01 -0400

    changed some code based on classmate feedback:

--- 14_form/app.py
+++ 14_form/app.py
@@ -48,10 +48,15 @@ def authenticate():
     # print(request)
     # print("***DIAG: request.args ***")
     # print(request.args)
+
+    #prints username and headers in the terminal
     print("***DIAG: request.args['username']  ***")
     print(request.args['username'])
     print("***DIAG: request.headers ***")
     print(request.headers)
+
+    #authenicate will go to the response page which will use response.html
+    #we set username here so that it can be used in response.html in our greeting
     return render_template( 'response.html', username=request.args['username'])  #response to a form submission
 
 
