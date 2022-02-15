61923439c5eed9abbb0f1e8d0477d7cbff0c00d8
ellakrechmer <ekrechmer20@stuy.edu>
2022-02-14 22:15:49 -0500

    k30

---
+++ 30_jsdraw/clickdraw.js
@@ -0,0 +1,50 @@
+// EELs :: Eliza Knapp, Ella Krechmer, Lucas Lee
+// SoftDev pd0
+// K30 -- canvas with JS
+// 2022-02-014
+
+var c = document.getElementById("slate");
+var ctx = c.getContext("2d");
+var mode = "rect";
+
+var toggleMode = (e) => {
+    console.log("toggle");
+    if(mode === "rect") mode = "circ";
+    else mode = "rect";
+}
+
+var drawRect = (e) => {
+    console.log("draw rect");
+    mouseX = e.offsetX;
+    mouseY = e.offsetY;
+    ctx.fillStyle = "red";
+    ctx.fillRect(mouseX, mouseY, 75, 150);
+}
+
+var drawCircle = (e) => {
+    console.log("draw circ");
+    mouseX = e.offsetX;
+    mouseY = e.offsetY;
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(mouseX, mouseY, 50, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
+var draw = (e) => {
+    console.log("drawing");
+    mode === "rect" ? drawRect(e) : drawCircle(e);
+}
+
+var wipe = (e) => {
+    console.log("wipe");
+    ctx.clearRect(0, 0, c.width, c.height);
+}
+
+c.addEventListener("click", draw);
+
+var bToggler = document.getElementById("buttonToggle");
+bToggler.addEventListener("click", toggleMode);
+
+var clearB = document.getElementById("buttonClear");
+clearB.addEventListener("click", wipe);
